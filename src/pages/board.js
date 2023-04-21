acbb5ca010244e7dc10a0ae7673076f9c3849108
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-04-21 18:56:33 +0900

    보드판 생성

--- src/pages/board.js
+++ src/pages/board.js
@@ -7,6 +7,16 @@ export default function Board({tiles}) {
             {tiles.map((tile) => (
                 <Tile number={tile} />
             ))}
+            <style jsx>{`
+                .board {
+                display: grid;
+                grid-template-columns: repeat(4, 1fr);
+                grid-template-rows: repeat(4, 1fr);
+                gap: 100px;
+                padding: 100px;
+                background-color: beige;
+                }
+            `}</style>
         </div>
     );
 }
