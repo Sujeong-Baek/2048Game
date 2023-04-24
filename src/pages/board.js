2691d4bb4f41b9b6444f514a12ea881d4256d58b
Sujeong-Baek <122095491+Sujeong-Baek@users.noreply.github.com>
2023-04-24 15:24:07 +0900

    tile merge

--- src/pages/board.js
+++ src/pages/board.js
@@ -54,13 +54,75 @@ export default function Board() {
     }
 
     function push_down() {
-    }
+        for (let col = 0; col < 4; col++) {
+          let compare_num = 0;
+          let pos = 3;
+          for (let row = 3; row >= 0; row--) {
+            if (boardTiles[4 * row + col] === 0) {
+              continue;
+            }
+            if (compare_num !== boardTiles[4 * row + col]) {
+              compare_num = boardTiles[4 * row + col];
+              boardTiles[4 * row + col] = 0;
+              boardTiles[4 * pos + col] = compare_num;
+              pos--;
+            } else if (compare_num === boardTiles[4 * row + col]) {
+              boardTiles[4 * (pos + 1) + col] = compare_num * 2;
+              boardTiles[4 * row + col] = 0;
+              compare_num = 0;
+              pos--;
+            }
+          }
+        }
+        setBoardTiles([...boardTiles]);
+      }
+      
+      function push_right() {
+        for (let row = 0; row < 4; row++) {
+          let compare_num = 0;
+          let pos = 3;
+          for (let col = 3; col >= 0; col--) {
+            if (boardTiles[4 * row + col] === 0) {
+              continue;
+            }
+            if (compare_num !== boardTiles[4 * row + col]) {
+              compare_num = boardTiles[4 * row + col];
+              boardTiles[4 * row + col] = 0;
+              boardTiles[4 * row + pos] = compare_num;
+              pos--;
+            } else if (compare_num === boardTiles[4 * row + col]) {
+              boardTiles[4 * row + pos + 1] = compare_num * 2;
+              boardTiles[4 * row + col] = 0;
+              compare_num = 0;
+              pos--;
+            }
+          }
+        }
+        setBoardTiles([...boardTiles]);
+      }
 
-    function push_right() {
-    }
-
-    function push_up() {
-    }
+      function push_up() {
+        for (let col = 0; col < 4; col++) {
+          let compare_num = 0;
+          let pos = 0;
+          for (let row = 0; row < 4; row++) {
+            if (boardTiles[4 * row + col] === 0) {
+              continue;
+            }
+            if (compare_num !== boardTiles[4 * row + col]) {
+              compare_num = boardTiles[4 * row + col];
+              boardTiles[4 * row + col] = 0;
+              boardTiles[4 * pos + col] = compare_num;
+              pos++;
+            } else if (compare_num === boardTiles[4 * row + col]) {
+              boardTiles[4 * (pos - 1) + col] = compare_num * 2;
+              boardTiles[4 * row + col] = 0;
+              compare_num = 0;
+            }
+          }
+        }
+        setBoardTiles([...boardTiles]);
+      }
 
     function handleKeyDown(event) {
         if (event.key === "ArrowLeft") {
@@ -75,10 +137,12 @@ export default function Board() {
     }
 
     return (
-        <div className="board">
+        <div  onKeyDown={handleKeyDown} tabIndex="0">
+            <div className="board">
             {boardTiles.map((tile,index) => (
-                <Tile number={tile} key={index}  />
+                <Tile number={tile} key={index} />
             ))}
+            </div>
         </div>
-    );
+      );
 }
